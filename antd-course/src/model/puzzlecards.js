599014306e816b2e4e40b09e11a5863ca0d1bd16
cuzz <cuzz1234@163.com>
2019-07-18 21:33:22 +0800

    antd

--- antd-course/src/model/puzzlecards.js
+++ antd-course/src/model/puzzlecards.js
@@ -1,21 +1,38 @@
+import request from '../util/request'; // request 是 demo 项目脚手架中提供的一个做 http 请求的方法，是对于 fetch 的封装，返回 Promise
+
+const delay = (millisecond) => {
+    return new Promise((resolve) => {
+        setTimeout(resolve, millisecond);
+    });
+};
+
+
+
+
+
 export default {
     namespace: 'puzzlecards',
     state: {
-        data: [
-            {
-                id: 1,
-                setup: 'Did you hear about the two silk worms in a race?',
-                punchline: 'It ended in a tie',
-            },
-            {
-                id: 2,
-                setup: 'What happens to a frog\'s car when it breaks down?',
-                punchline: 'It gets toad away',
-            },
-        ],
-        counter: 100,
+        data: [],
+        counter: 0,
     },
+    effects: {
+        *queryInitCards(_, sagaEffects) { // 定义异步方法
+            const { call, put } = sagaEffects; // 获取 call 和 put 方法
+            const endPointURI = 'https://safe-falls-22549.herokuapp.com/random_joke'; // 请求 url
+
+            const puzzle = yield call(request, endPointURI); // 获取数据
+            yield put({ type: 'addNewCard', payload: puzzle });
+
+            yield call(delay, 3000);
+
+            const puzzle2 = yield call(request, endPointURI);
+            yield put({ type: 'addNewCard', payload: puzzle2 }); // 通过 put 调用 reducers 中的函数
+        }
+    },
+    // 定义一些函数
     reducers: {
+        // state: 指的是更新之前的状态数据
         addNewCard(state, { payload: newCard }) {
             const nextCounter = state.counter + 1;
             const newCardWithId = { ...newCard, id: nextCounter };
--- antd-course/src/page/puzzlecards.js
+++ antd-course/src/page/puzzlecards.js
@@ -4,6 +4,7 @@ import { connect } from 'dva';
 
 const namespace = 'puzzlecards';
 
+// 作用：将 page 层和 model 层进行链接，返回 model 层中数据，并且将返回的数据绑定到 this.props
 const mapStateToProps = (state) => {
     const cardList = state[namespace].data;
     return {
@@ -11,14 +12,22 @@ const mapStateToProps = (state) => {
     };
 };
 
-const mapDispatchToProps = (dispatch) => {
+// 作用：将定义的函数绑定到 this.props 中，调用 model 层定义的函数
+const mapDispatchToProps = (dispatch) => { // dispatch 的作用：可以调用 model 层定义的函数
+    // return {
+    //     onClickAdd: (newCard) => { // 将函数绑定到 this.props 中
+    //         const action = {
+    //             // type: `${namespace}/addNewCard`, // 常量用 ` 号
+    //             payload: newCard,
+    //         };
+    //         dispatch(action); // 通过 dispatch 调用 model 中的函数
+    //     },
+    // };
     return {
-        onClickAdd: (newCard) => {
-            const action = {
-                type: `${namespace}/addNewCard`,
-                payload: newCard,
-            };
-            dispatch(action);
+        onDidMount: () => {
+            dispatch({
+                type: `${namespace}/queryInitCards`,
+            });
         },
     };
 };
@@ -26,6 +35,12 @@ const mapDispatchToProps = (dispatch) => {
 // 数据流向图 see https://gw.alipayobjects.com/zos/rmsportal/uhUMfTcXxfskqbreAXth.png
 @connect(mapStateToProps, mapDispatchToProps)
 export default class PuzzleCardsPage extends Component {
+
+    // 初始化完成之后，发送请求
+    componentDidMount() {
+        this.props.onDidMount();
+    }
+
     render() {
         return (
             <div>
